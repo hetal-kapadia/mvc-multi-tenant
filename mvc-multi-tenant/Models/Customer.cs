032d1c7e3a184b5d4b35e250fa7055f9f9068772
Hetal Kapadia <hetal.kapadiap@protonmail.com>
2021-05-29 18:26:09 -0700

    Added models.

---
+++ mvc-multi-tenant/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_multi_tenant.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public Guid TenantId { get; set; }
+        public string CustomerName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ mvc-multi-tenant/Models/Tenant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_multi_tenant.Models
+{
+    public class Tenant
+    {
+        public Guid Id { get; set; }
+        public Guid ApiKey { get; set; }
+        public string TenantName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
