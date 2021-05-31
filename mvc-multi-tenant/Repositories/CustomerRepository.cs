d3f85e8160d8796bd94c611ca35159c9e5ec0064
Hetal Kapadia <hetal.kapadiap@protonmail.com>
2021-05-30 18:10:15 -0700

    Added repository classes.

---
+++ mvc-multi-tenant/Repositories/CustomerRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using mvc_multi_tenant.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_multi_tenant.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomerRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<List<Customer>> GetAllCustomers(string tenantId)
+        {
+            try
+            {
+                List<Customer> customers = new List<Customer>();
+
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("TenantDbConnection")))
+                {
+                    await connection.OpenAsync();
+
+                    using (var command = new SqlCommand("SELECT * FROM Customers WHERE TenantId = @tenantId", connection))
+                    {
+                        command.Parameters.AddWithValue("@tenantId", tenantId);
+
+                        var reader = await command.ExecuteReaderAsync();
+                        if (reader.Read())
+                        {
+                            Customer customer = new Customer()
+                            {
+                                Id = Guid.Parse(reader["Id"].ToString()),
+                                TenantId = Guid.Parse(reader["TenantId"].ToString()),
+                                CustomerName = reader["Id"].ToString(),
+                                IsActive = bool.Parse(reader["IsActive"].ToString())
+                            };
+
+                            customers.Add(customer);
+                        }
+
+                        if (!reader.IsClosed)
+                            await reader.CloseAsync();
+                    }
+
+                    if (connection.State != ConnectionState.Closed)
+                        await connection.CloseAsync();
+
+                    return customers;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ mvc-multi-tenant/Repositories/TenantRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_multi_tenant.Repositories
+{
+    public class TenantRepository
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private ISession _session => _httpContextAccessor.HttpContext.Session;
+        
+        private readonly IConfiguration _configuration;
+        
+        public TenantRepository(IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        {
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<string> GetTenantId(Guid apiKey)
+        {
+            string tenantId = null;
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration["ConnectionStrings:TenantDBConnection"]))
+                {
+                    await connection.OpenAsync();
+
+                    using (var command = new SqlCommand("SELECT Id FROM Tenant WHERE ApiKey = @apiKey", connection))
+                    {
+                        command.Parameters.AddWithValue("@apiKey", apiKey);
+
+                        var reader = await command.ExecuteReaderAsync();
+                        if (reader.Read())
+                        {
+                            tenantId = reader["Id"].ToString();
+                        }
+
+                        if (!reader.IsClosed)
+                            await reader.CloseAsync();
+                    }
+
+                    if (connection.State != ConnectionState.Closed)
+                        await connection.CloseAsync();
+
+                    return tenantId;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> GetTenantId()
+        {
+            return await Task.FromResult(_session.GetString("TenantId"));
+        }
+    }
+}
